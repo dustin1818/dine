93005b15064603f3354c1e0d0141f4a9f24b8dea
dustin1818 <dustinamoda18@gmail.com>
2025-07-16 17:03:08 +0800

    add line break in alert message

--- reservation.js
+++ reservation.js
@@ -4,6 +4,6 @@ reservationForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
-  alert('Reservation successful!' + JSON.stringify(formProps))
+  alert("Reservation successful!  \n-------------------------\n" +  JSON.stringify(formProps))
   reservationForm.reset();
 });
