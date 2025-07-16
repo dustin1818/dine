5614850a72f440ea38117f2a790b4b62f511b585
dustin1818 <dustinamoda18@gmail.com>
2025-07-16 16:52:50 +0800

    initial commit

---
+++ reservation.js
@@ -0,0 +1,9 @@
+const reservationForm = document.getElementById("reservationForm");
+
+reservationForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formProps = Object.fromEntries(formData);
+  alert('Reservation successful!' + JSON.stringify(formProps))
+  reservationForm.reset();
+});
